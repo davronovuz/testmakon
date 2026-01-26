e10a03fd0ad05be8a8d38d9be48dacec3459a09a
davronovuz <davronovtatu@gmail.com>
2026-01-26 17:29:45 +0500

    asassasa saas sa aas

--- news/admin.py
+++ news/admin.py
@@ -1,40 +1,40 @@
-# from django.contrib import admin
-# from .models import Category, Article, ArticleLike, Notification
-#
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'slug', 'is_active', 'order')
-#     list_editable = ('is_active', 'order')
-#     prepopulated_fields = {'slug': ('name',)}
-#     search_fields = ('name',)
-#
-# @admin.register(Article)
-# class ArticleAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'category', 'article_type', 'views_count', 'is_published', 'published_at')
-#     list_filter = ('article_type', 'is_published', 'is_featured', 'category', 'created_at')
-#     search_fields = ('title', 'content')
-#     prepopulated_fields = {'slug': ('title',)}
-#     date_hierarchy = 'published_at'
-#     autocomplete_fields = ['author', 'subject']
-#     list_editable = ('is_published',)
-#
-# @admin.register(Notification)
-# class NotificationAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'user', 'notification_type', 'is_read', 'created_at')
-#     list_filter = ('notification_type', 'is_read', 'created_at')
-#     search_fields = ('title', 'message', 'user__first_name', 'user__phone_number')
-#     date_hierarchy = 'created_at'
-#
-# admin.site.register(ArticleLike)
-#
-#
-#
-# from .models import SystemBanner
-#
-# @admin.register(SystemBanner)
-# class SystemBannerAdmin(admin.ModelAdmin):
-#     list_display = ['message', 'banner_type', 'is_active', 'is_scrolling', 'order', 'created_at']
-#     list_filter = ['banner_type', 'is_active', 'is_scrolling']
-#     list_editable = ['is_active', 'order']
-#     search_fields = ['message']
-#     ordering = ['order', '-created_at']
+from django.contrib import admin
+from .models import Category, Article, ArticleLike, Notification
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'is_active', 'order')
+    list_editable = ('is_active', 'order')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'article_type', 'views_count', 'is_published', 'published_at')
+    list_filter = ('article_type', 'is_published', 'is_featured', 'category', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_at'
+    autocomplete_fields = ['author', 'subject']
+    list_editable = ('is_published',)
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'notification_type', 'is_read', 'created_at')
+    list_filter = ('notification_type', 'is_read', 'created_at')
+    search_fields = ('title', 'message', 'user__first_name', 'user__phone_number')
+    date_hierarchy = 'created_at'
+
+admin.site.register(ArticleLike)
+
+
+
+from .models import SystemBanner
+
+@admin.register(SystemBanner)
+class SystemBannerAdmin(admin.ModelAdmin):
+    list_display = ['message', 'banner_type', 'is_active', 'is_scrolling', 'order', 'created_at']
+    list_filter = ['banner_type', 'is_active', 'is_scrolling']
+    list_editable = ['is_active', 'order']
+    search_fields = ['message']
+    ordering = ['order', '-created_at']
